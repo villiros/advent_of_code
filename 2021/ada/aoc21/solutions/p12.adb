82bc8d1bdd88d50d7078feb4c9b149ce1d715704
Vlad <me@villiros.com>
2021-12-12 08:32:12 +0000

    Day 12, faster.

--- 2021/ada/aoc21/solutions/p12.adb
+++ 2021/ada/aoc21/solutions/p12.adb
@@ -26,48 +26,50 @@ package body p12 is
     -- Common stuff
     ------------------------------------------------------------------------
 
+    ShouldNotGetHere: exception;
+
+    -- Original solution used indefinite hashed maps and sets and was pretty
+    -- slow (400ms on B).
+    --
+    -- There are very few caves, so instead of using string-based maps,
+    -- the cave names are converted into numbers and the whole thing can be
+    -- kept in arrays.
+    --
+    -- Each node contains a (CaveID) -> IsConnected array; simply
+    -- iterating over the whole thing on each step is fairly fast.
+    -- *Could* be optimized to keep connected as a bounded array, but meh.
+
+    -- IDs for caves. One of them is not a valid cave.
+    type CaveId is range 1..20;
+    -- Actual caves
+    subtype CaveNum is CaveId range 1..(CaveId'Pred(CaveId'Last));
+    -- Null marker
+    subtype NotACaveNum is CaveId range CaveId'Last..CaveId'Last;
+    NotACave: constant CaveId := NotACaveNum'Last;
+
+    -- Still need to track the actual name
     subtype NameLen is Integer range 1..5;
     subtype NameType is String(NameLen);
 
-    StartName: constant NameType := "start     ";
-    EndName  : constant NameType := "end       ";
-    NoCaveName : constant NameType := "NOTACAVE  ";
+    StartName: constant NameType := "start";
+    EndName  : constant NameType := "end  ";
 
-    package ConnectsPkg is
-        new Ada.Containers.Vectors(Index_type => Positive, Element_Type => NameType);
-    use ConnectsPkg;
-    subtype ConnectsType is ConnectsPkg.Vector;
-
-    
-    type CaveType is (Cstart, Cend, Cbig, Csmall);
+    type CaveType is (Cstart, Cend, Cbig, Csmall, NoCave);
+    -- Connections from one cave to another (index)
+    type Connects is array(CaveNum) of Boolean;
 
     type Cave is record
         name: NameType;
-        ct: CaveType;
-        conn: ConnectsType;
+        ct: CaveType := NoCave;
+        conn: Connects := (others => False);
     end record;
-
-    package CavesPkg is new
-        Ada.Containers.Indefinite_Hashed_Maps(
-            Key_Type => NameType,
-            Element_type => Cave,
-            Hash => Ada.Strings.Hash,
-            Equivalent_Keys => "="
-            );
-    use CavesPkg;
-    subtype CavesType is CavesPkg.Map;
 
     ------------------------------------
     -- Base solution and data reading
     ------------------------------------
 
-    package VisitedPkg is new
-        Ada.Containers.Indefinite_Hashed_Sets(
-            Element_Type => NameType,
-            hash => Ada.Strings.Hash,
-            Equivalent_Elements => "=");
-    use VisitedPkg;
-    subtype VisitedType is VisitedPkg.Set;
+    type VisitedType is array(CaveNum) of Boolean;
+    type CavesType is array(CaveNum) of Cave;
 
     function ToCaveType(name: NameType) return CaveType is
         use Ada.Characters.Handling;
@@ -87,42 +89,62 @@ package body p12 is
         Input : CavesType;
     end record;
 
+    -- Returns index of next empty cell if name not known
+    function GetIndex(s: Solution'Class; name: NameType) return CaveNum is
+    begin
+        for I in CaveNum loop
+            if s.Input(I).name = name or
+                s.Input(I).ct = NoCave then
+                return I;
+            end if;
+        end loop;
+        raise ShouldNotGetHere;
+    end GetIndex;
+
     procedure ReadInput(self : in out Solution'class; InData : File_Type)
     with
         Post =>
-            (not self.Input.Is_Empty) and
-            (self.Input.Contains(StartName)) and
-            (self.Input.Contains(EndName)) and
-            (for all E of self.Input =>
-                not E.conn.Is_Empty and
-                (for all E2 of E.conn =>
-                    self.Input.Contains(E2) and
-                    self.Input.Element(E2).conn.Contains(E.name)))
+            -- Start and end loaded
+            (for some I in CaveNum => self.Input(I).ct = Cstart) and
+            (for some I in CaveNum => self.Input(I).ct = Cend) and
+            -- No overflow
+            (self.Input(CaveNum'Last).ct = NoCave) and
+            -- Check connectivity
+            (for all I in CaveNum =>
+                (if self.Input(I).ct /= NoCave then
+                    -- Must connect to something
+                    (for some J in CaveNum => self.Input(I).conn(J)) and
+                    -- All connections are bidirectional
+                    (for all J in CaveNum =>
+                        (if self.Input(I).conn(J) then
+                            self.Input(J).ct /= NoCave and
+                            self.Input(J).conn(I)))))
     is
         use Ada.Strings.Fixed;
     begin
         while not End_Of_File(InData) loop
             declare
-                from: NameType;
-                to: NameType;
+                from, to: NameType;
+                fromIndex, toIndex: CaveNum;
             begin
                 Move(InputStrPkg.To_String(GetAtom (InData, Ada.Strings.Maps.To_Set("-"))), from);
                 SkipChar (InData, '-');
                 Move(InputStrPkg.To_String(GetAtom (InData)), to);
 
-                if not self.Input.Contains (from) then
-                    self.Input.Include (from, (name => from,
-                                               ct => ToCaveType (from),
-                                               others => <>));
-                end if;
-                if not self.Input.Contains (to) then
-                    self.Input.Include (to, (name => to,
-                                             ct => ToCaveType (to),
-                                             others => <>));
+                fromIndex := GetIndex (self, from);
+                if self.Input(fromIndex).ct = NoCave then
+                    self.Input(fromIndex).name := from;
+                    self.Input(fromIndex).ct := ToCaveType (from);
                 end if;
 
-                self.Input(from).conn.Append (to);
-                self.Input(to).conn.Append (from);
+                toIndex := GetIndex (self, to);
+                if self.Input(toIndex).ct = NoCave then
+                    self.Input(toIndex).name := to;
+                    self.Input(toIndex).ct := ToCaveType (to);
+                end if;
+
+                self.Input(fromIndex).conn(toIndex) := True;
+                self.Input(toIndex).conn(fromIndex) := True;
             end;
             AdvanceLine (InData);
         end loop;
@@ -132,31 +154,27 @@ package body p12 is
     -- Part A
     ------------------------------------------------------------------------
 
-    function Visitor(m: CavesType; v: in out VisitedType;
-                     curName: NameType;
-                     numPaths: in out ResultType) return Boolean is
-        cur: Cave renames m.Element(curName);
-
-        hadPaths: Boolean := False;
+    procedure Visitor(m: CavesType; v: in out VisitedType;
+                      curId: CaveNum;
+                      numPaths: in out ResultType) is
+        cur: Cave renames m(curId);
     begin
         if cur.ct = Cend then
             numPaths := numPaths + 1;
-            return True;
+            return;
         end if;
 
-        for NName of cur.conn loop
-            if not v.Contains (NName) then
-                if m.Element(NName).ct /= Cbig then
-                    v.Include (NName);
+        for NextId in CaveNum loop
+            if cur.conn(NextId) and not v(NextId) then
+                if m(NextId).ct /= Cbig then
+                    v(NextId) := True;
                 end if;
 
-                hadPaths := hadPaths or Visitor(m, v, NName, numPaths);
+                Visitor(m, v, NextId, numPaths);
 
-                v.Exclude (NName);
+                v(NextId) := False;
             end if;
         end loop;
-
-        return hadPaths;
     end Visitor;
 
     type SolutionA is new Solution with record
@@ -168,18 +186,14 @@ package body p12 is
                     StartTs : out Ada.Real_Time.Time) return ResultType is
         s : SolutionA;
 
-        v: VisitedType;
+        v: VisitedType := (others => False);
         result: ResultType := 0;
     begin
         ReadInput(s, InData);
         StartTs := Ada.Real_Time.Clock;
 
-        v.Include(StartName);
-        declare
-            junk: Boolean := Visitor(s.Input, v, StartName, result);
-        begin
-            null;
-        end;
+        v(GetIndex(s, StartName)) := True;
+        Visitor(s.Input, v, GetIndex(s, StartName), result);
 
         return result;
     end Solve;
@@ -188,40 +202,37 @@ package body p12 is
     -- Part B
     ------------------------------------------------------------------------
 
-    function VisitorB(m: CavesType; v: in out VisitedType; twiceVisit: NameType;
-                      curName: NameType;
-                      numPaths: in out ResultType) return Boolean is
-        cur: Cave renames m.Element(curName);
-
-        hadPaths: Boolean := False;
+    procedure VisitorB(m: CavesType; v: in out VisitedType; twiceVisit: CaveId;
+                       curId: CaveNum;
+                       numPaths: in out ResultType) is
+        cur: Cave renames m(curId);
     begin
         if cur.ct = Cend then
             numPaths := numPaths + 1;
-            return True;
+            return;
         end if;
 
-        for NName of cur.conn loop
-            if (not v.Contains (NName)) or else
-               (twiceVisit = NoCaveName and NName /= StartName) then
+        for NextId in CaveNum loop
+            if cur.conn(NextId) and
+               ((not v(NextId)) or else
+                (twiceVisit = NotACave and m(NextId).ct /= Cstart)) then
+
                 declare
-                    alreadyVisited: Boolean := v.Contains (NName);
-                    -- If already visited, then twiceVisit must've been NoCaveName
-                    twiceVisitNew: NameType := (if alreadyVisited then NName else twiceVisit);
+                    alreadyVisited: Boolean := v(NextId);
+                    twiceVisitNew: CaveId := (if alreadyVisited then NextId else twiceVisit);
                 begin
-                    if m.Element(NName).ct /= Cbig then
-                        v.Include (NName);
+                    if m(NextId).ct /= Cbig then
+                        v(NextId) := True;
                     end if;
 
-                    hadPaths := hadPaths or VisitorB(m, v, twiceVisitNew, NName, numPaths);
+                    VisitorB(m, v, twiceVisitNew, NextId, numPaths);
 
                     if not alreadyVisited then
-                        v.Exclude (NName);
+                        v(NextId) := False;
                     end if;
                 end;
             end if;
         end loop;
-
-        return hadPaths;
     end VisitorB;
 
     type SolutionB is new Solution with record
@@ -232,18 +243,14 @@ package body p12 is
                     InData : in Ada.Text_IO.File_type;
                     StartTs : out Ada.Real_Time.Time) return ResultType is
         s : SolutionB;
-        v: VisitedType;
+        v: VisitedType := (others => False);
         result: ResultType := 0;
     begin
         ReadInput(s, InData);
         StartTs := Ada.Real_Time.Clock;
 
-        v.Include(StartName);
-        declare
-            junk: Boolean := VisitorB(s.Input, v, NoCaveName, StartName, result);
-        begin
-            null;
-        end;
+        v(GetIndex(s, StartName)) := True;
+        VisitorB(s.Input, v, NotACave, GetIndex(s, StartName), result);
 
         return result;
     end Solve;
